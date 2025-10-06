a7c054a5adf70879154ed42ece777a298aee61d7
Bhanu <bhanutumpala1998@gmail.com>
2025-10-06 22:02:46 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/lloydsbank/consumer/ConsumerServicingEngagementApplication.java
@@ -0,0 +1,13 @@
+package com.lloydsbank.consumer_servicing_engagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerServicingEngagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerServicingEngagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lloydsbank/consumer_servicing_engagement/ConsumerServicingEngagementApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.lloydsbank.consumer_servicing_engagement;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class ConsumerServicingEngagementApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
